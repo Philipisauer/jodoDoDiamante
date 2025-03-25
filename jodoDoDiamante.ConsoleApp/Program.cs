162e8d9a4b9b97069bcd8b559e927af23ca97d5a
Philipi Sauer <philipisauer09@gmail.com>
2025-03-25 17:41:33 -0300

    tentativa do trabalho kkk

--- jodoDoDiamante.ConsoleApp/Program.cs
+++ jodoDoDiamante.ConsoleApp/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace jodoDoDiamante.ConsoleApp
+﻿using System.Data.SqlTypes;
+
+namespace jodoDoDiamante.ConsoleApp
 {
     internal class Program
     {
+        //
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("~~~ Seja bem-vindo ao jogo dos diamante ~~~");
+
+                Console.WriteLine("Informe o número IMPAR desejado: ");
+                int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+                int numeroDeEspacos = numeroDigitado;
+
+
+                if (numeroDigitado == 1)
+                {
+                    Console.WriteLine("Diamante: ");
+                    Console.WriteLine("x");
+                }
+
+                if (numeroDigitado == 3)
+                {
+                    Console.WriteLine("Diamante: ");
+                    Console.WriteLine(" x");
+                    Console.WriteLine("xxx");
+                    Console.WriteLine(" x");
+                }
+
+                if (numeroDigitado == 5)
+                {
+                    Console.WriteLine("Diamante: ");
+                    Console.WriteLine("  x");
+                    Console.WriteLine(" xxx");
+                    Console.WriteLine("xxxxx");
+                    Console.WriteLine(" xxx");
+                    Console.WriteLine("  x");
+                }
+
+
+
+                Console.WriteLine("deseja continuar (S/N)");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+                if (opcaoContinuar != "S")
+                    break;
+            }   
         }
     }
 }
